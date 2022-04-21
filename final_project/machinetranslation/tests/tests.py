a81342ca6f659da5b35d09b459aa5f700de1fbae
Aldy Febriansyah <aldifebri25@gmail.com>
2022-04-21 17:53:02 +0000

    final commit

---
+++ final_project/machinetranslation/tests/tests.py
@@ -0,0 +1,19 @@
+import unittest
+
+from translator import english_to_french, french_to_english
+
+class TestTranslator(unittest.TestCase):
+    def test_french_to_english_null_input(self):
+        self.assertNotEqual(french_to_english('Bonjour'),None)
+        
+    def test_english_to_french_null_input(self):
+        self.assertNotEqual(english_to_french('Hello'),None)
+    
+    def test_french_to_english(self):
+        self.assertEqual(french_to_english('Bonjour'),'Hello')
+        
+    def test_english_to_french(self):
+        self.assertEqual(english_to_french('Hello'),'Bonjour')
+        
+if __name__ == '__main__':
+    unittest.main()
