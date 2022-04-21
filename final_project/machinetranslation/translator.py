fd38a8c15a7641dbfd9dc6af3353c8656d8917b2
Aldy <aldifebri25@gmail.com>
2022-04-22 01:23:57 +0700

    Fixed

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -18,14 +18,12 @@ language_translator = LanguageTranslatorV3(
 language_translator.set_service_url(url)
 
 def english_to_french(english_text):
-    translation_response = language_translator.translate(text = english_text, 
-        model_id='en-fr')
+    translation_response = language_translator.translate(text = english_text, model_id='en-fr')
     translation = translation_response.get_result()
     french_text = translation['translations'][0]['translation']
     return french_text
 def french_to_english(french_text):
-    translation_response = language_translator.translate(text = french_text, 
-        model_id='fr-en')
+    translation_response = language_translator.translate(text = french_text, model_id='fr-en')
     translation = translation_response.get_result()
     english_text = translation['translations'][0]['translation']
     return english_text
